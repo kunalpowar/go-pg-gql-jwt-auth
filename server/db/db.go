9d1f6c01299f037083ab2aeadbc33ff06c08af0e
Kunal U Powar <kunalpowar1203@gmail.com>
2020-12-07 22:51:40 +0530

    server: removed unwanted code

--- server/db/db.go
+++ server/db/db.go
@@ -12,30 +12,8 @@ var Instance *pg.DB
 
 type InitDBOpts pg.Options
 
-const (
-	defAddres   = "localhost:5432"
-	defDBUser   = "kunalpowar"
-	defDatabase = "gopggqlauth"
-)
-
-func (o *InitDBOpts) useDefaultsIfEmpty() {
-	if o.Addr == "" {
-		o.Addr = defAddres
-	}
-
-	if o.User == "" {
-		o.User = defDBUser
-	}
-
-	if o.Database == "" {
-		o.Database = defDBUser
-	}
-}
-
 // MustInit connects to the db and initialises the global DB var.
 func MustInit(opts *InitDBOpts) {
-	opts.useDefaultsIfEmpty()
-
 	Instance = pg.Connect(&pg.Options{
 		User:     opts.User,
 		Database: opts.Database,
