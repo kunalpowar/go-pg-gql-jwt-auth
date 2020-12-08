213a1e520a2da9aecbb943b4d65e739cce102399
Kunal U Powar <kunalpowar1203@gmail.com>
2020-12-08 13:11:18 +0530

    server: refactor db initialisation

--- server/db/db.go
+++ server/db/db.go
@@ -1,7 +1,10 @@
 package db
 
 import (
+	"context"
+	"fmt"
 	"log"
+	"os"
 
 	"github.com/go-pg/pg/v10"
 )
@@ -9,14 +12,37 @@ import (
 // Instance refers to the connected db.
 var Instance *pg.DB
 
-type InitDBOpts pg.Options
-
 // MustInit connects to the db and initialises the global DB var.
-func MustInit(opts *InitDBOpts) {
-	Instance = pg.Connect(&pg.Options{
-		User:     opts.User,
-		Database: opts.Database,
-	})
+func MustInit(ctx context.Context) {
+	opts := pg.Options{
+		User:     os.Getenv("DB_USER"),
+		Password: os.Getenv("DB_PASS"),
+		Database: os.Getenv("DB_NAME"),
+	}
 
-	log.Printf("connected to DB: %v", Instance)
+	tcpHost := os.Getenv("DB_TCP_HOST")
+	if tcpHost != "" {
+		opts.Network = "tcp"
+		opts.Addr = tcpHost
+
+		log.Printf("db: attempting to connect to db via tcp: %s", opts.Addr)
+	} else {
+		dbSocketName := os.Getenv("INSTANCE_CONNECTION_NAME")
+		if dbSocketName == "" {
+			log.Fatalf("db: at least one of DB_TCP_HOST or INSTANCE_CONNECTION_NAME should be set in env")
+		}
+
+		opts.Network = "unix"
+		opts.Addr = fmt.Sprintf("/cloudsql/%s/.s.PGSQL.5432", dbSocketName)
+
+		log.Printf("db: attempting to connect to db via unit socket: %s", opts.Addr)
+	}
+
+	Instance = pg.Connect(&opts)
+
+	if err := Instance.Ping(ctx); err != nil {
+		log.Fatalf("db: could not ping db: %v", err)
+	}
+
+	log.Printf("db: connection established: %v", Instance)
 }
--- server/server.go
+++ server/server.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"fmt"
+	"context"
 	"log"
 	"net/http"
 	"os"
@@ -19,31 +19,8 @@ import (
 const defaultPort = "8080"
 
 func main() {
-	opts := db.InitDBOpts{
-		User:     os.Getenv("DB_USER"),
-		Password: os.Getenv("DB_PASS"),
-		Database: os.Getenv("DB_NAME"),
-	}
-
-	tcpHost := os.Getenv("DB_TCP_HOST")
-	if tcpHost != "" {
-		opts.Network = "tcp"
-		opts.Addr = tcpHost
-
-		log.Printf("attempting to connect to db via tcp: %s", opts.Addr)
-	} else {
-		dbSocketName := os.Getenv("INSTANCE_CONNECTION_NAME")
-		if dbSocketName == "" {
-			log.Fatalf("at least one of DB_TCP_HOST or INSTANCE_CONNECTION_NAME should be set in env")
-		}
-
-		opts.Network = "unix"
-		opts.Addr = fmt.Sprintf("/cloudsql/%s/.s.PGSQL.5432", dbSocketName)
-
-		log.Printf("attempting to connect to db via unit socket: %s", opts.Addr)
-	}
-
-	db.MustInit(&opts)
+	ctx := context.Background()
+	db.MustInit(ctx)
 
 	router := chi.NewRouter()
 	router.Use(auth.Middleware())
